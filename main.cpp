2effdde5837d8f42c73df4fbf2566464f730fbea
MosalovK <kirillmosalovv@gmail.com>
2020-05-15 14:34:42 +0300

    code: ввод через структуры

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@
 
 using namespace std;
 
+struct Input
+{
+    vector <double> numbers;
+    size_t bin_count;
+};
+
 vector<double> input_numbers(istream& in, size_t count)
 {
     vector<double> result(count);
@@ -12,6 +18,21 @@ vector<double> input_numbers(istream& in, size_t count)
         in >> result[i];
     }
     return result;
+}
+
+Input read_input(istream& in)
+{
+    Input data;
+    cerr << "Enter number count: ";
+    size_t number_count;
+    cin >> number_count;
+    cerr << "Enter numbers: ";
+    data.numbers = input_numbers(in, number_count);
+    cerr << "Vvedite kol-vo korzin: ";
+    size_t bin_count;
+    cin >> bin_count;
+    data.bin_count = bin_count;
+    return data;
 }
 
 /*
@@ -52,14 +73,9 @@ void show_histogram_text (const vector<size_t>& bins)
 
 int main()
 {
-    cerr << "Vvedite kol-vo chisel: ";
-    size_t number_count;
-    cin >> number_count;
-    const auto numbers = input_numbers(cin, number_count);
-    cerr << "Vvedite kol-vo korzin: ";
-    size_t bin_count;
-    cin >> bin_count;
-    const auto bins = make_histogram(numbers, bin_count);
+
+    const auto input = read_input(cin);
+    const auto bins = make_histogram(input.numbers, input.bin_count);
     if (bins.size() == 0)
     {
         cout << "error";
